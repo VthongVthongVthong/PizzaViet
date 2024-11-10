94441af03360e89a247b1ab21c191daa87252ecc
VthongVthongVthong <huynhvienthong979@gmail.com>
2024-11-10 18:11:33 +0700

    Modify Hard Password method

--- js/login.js
+++ js/login.js
@@ -48,41 +48,36 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function login(e) {
-    e.preventDefault();
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var user = localStorage.getItem(username);
-    var data = JSON.parse(user);
-    if(user == null){
-        Swal.fire({
-            title: 'Lỗi!',
-            text: 'Tên đăng nhập không tồn tại',
-            icon: 'error',
-            confirmButtonText: 'Thử lại'
-          })
-    }
-    else if(username==data.username && password== data.password){
-        Swal.fire({
-            title: 'Thành công!',
-            text: 'Đăng nhập thành công',
-            icon: 'success',
-            confirmButtonText: 'Chuyển tới trang web'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              localStorage.setItem("username", username);
-              window.location.href = './pages_user/Pizza website_user.html';
-            }
-          });
-        
-    }
-    else{
-        Swal.fire({
-            title: 'Lỗi!',
-            text: 'Mật khẩu không chính xác',
-            icon: 'error',
-            confirmButtonText: 'Thử lại'
-          })
-    }
+  e.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if(username === "khachhang" && password === "khachhang123"){
+    Swal.fire({
+      title: 'Thành công!',
+      text: 'Đăng nhập thành công',
+      icon: 'success',
+      confirmButtonText: 'Chuyển tới trang web'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = './pages_user/Pizza website_user.html';
+      }
+    });
+  } else if (username !== "khachhang") {
+    Swal.fire({
+      title: 'Lỗi!',
+      text: 'Tên đăng nhập không tồn tại',
+      icon: 'error',
+      confirmButtonText: 'Thử lại'
+    });
+  } else {
+    Swal.fire({
+      title: 'Lỗi!',
+      text: 'Mật khẩu không chính xác',
+      icon: 'error',
+      confirmButtonText: 'Thử lại'
+    });
+  }
 }
 
 window.setInterval(() => {
