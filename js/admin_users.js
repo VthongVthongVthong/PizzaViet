239277b4886c287fe205b704c815162cdc3baa2a
hoangbao2908 <vohoangbao082005@gmail.com>
2024-11-13 13:13:06 +0700

    Add files via upload

--- js/admin_users.js
+++ js/admin_users.js
@@ -1,26 +1,51 @@
-
 function lockUser(button) {
     const isLocked = button.getAttribute("data-locked") === "true";
+    const row = button.closest('tr');
+    const deleteButton = row.querySelector('.delete-btn');
+    const editButton = row.querySelector('.edit');
 
     if (isLocked) {
         if (confirm("Bạn có chắc muốn mở khóa người dùng này không?")) {
-            button.innerHTML = "<i class='bx bxs-lock-open-alt' ></i>";
+            button.innerHTML = "<i class='bx bxs-lock-open-alt'></i>";
             button.setAttribute("data-locked", "false");
         }
     } else {
         if (confirm("Bạn có chắc muốn khóa người dùng này không?")) {
             button.innerHTML = "<i class='bx bxs-lock-alt'></i>";
             button.setAttribute("data-locked", "true");
+
         }
     }
 }
-// Xoa nguoi dung
+
 function deleteUser(button) {
+    const row = button.closest('tr');
+    const lockButton = row.querySelector('.lock');
+
+    if (lockButton.getAttribute("data-locked") === "true") {
+        alert("Vui lòng mở khóa để thực hiện hành động này.");
+        return;
+    }
+
     if (confirm("Bạn có chắc chắn muốn xóa người dùng này?")) {
-        const row = button.closest('tr');
         row.parentNode.removeChild(row);
     }
 }
+
+function openPopupUser(button) {
+    const row = button.closest('tr');
+    const lockButton = row.querySelector('.lock');
+
+    if (lockButton.getAttribute("data-locked") === "true") {
+        alert("Vui lòng mở khóa để chỉnh sửa.");
+        return;
+    }
+
+    document.getElementById('modal_container').style.display = 'block';
+}
+
+
+
 
 function toggleAllUsers(source) {
     const checkboxes = document.querySelectorAll('#Users_Table .product-checkbox');
@@ -42,6 +67,5 @@ function deleteSelectedUsers() {
     }
 }
 
-function openPopup() {
-    document.getElementById('modal_container').style.display = 'block';
-}
+
+
