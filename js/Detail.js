7073f764947f118c38b381d4de8bee734e44e4f2
VthongVthongVthong <huynhvienthong979@gmail.com>
2024-11-27 10:44:39 +0700

    Add files via upload

--- js/Detail.js
+++ js/Detail.js
@@ -54,7 +54,7 @@ const quantityInput = document.getElementById('soluong'); // Số lượng nhậ
             }).then((result) => {
                 if (result.isConfirmed) {
                     // Chuyển hướng tới trang login.html sau khi nhấn OK
-                    window.location.href = "../../../login.html";
+                    window.location.href = "../../login.html";
                 }
             });
         });
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }).then((result) => {
                 if (result.isConfirmed) {
                     // Chuyển hướng tới trang login.html sau khi nhấn OK
-                    window.location.href = "../../../login.html";
+                    window.location.href = "../../login.html";
                 }
             });
         });
